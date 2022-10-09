32be354d5b8068ab50242c5c04dbc32663ab913b
Dmitry <free-doom46@mail.ru>
2022-10-09 22:33:17 +0300

    adds function to find getRandomRositiveInterger/Float

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 
-adfgfg
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result =  Math.random() * (upper - lower + 1) + lower;
+  return result;
+};
+
+console.log(getRandomPositiveInteger(5, 10));
+
+
+const getRandomPositiveFloat = (a, b, digits) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower) + lower;
+  return +result.toFixed(digits);
+};
+
+console.log(getRandomPositiveFloat(1, 200, 5));
